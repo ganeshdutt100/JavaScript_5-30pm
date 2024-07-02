e63b52358898950bef4f64c02e358ab95cbcb402
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-02 19:02:01 +0530

    Variable

---
+++ js/class1.js
@@ -0,0 +1,75 @@
+  //  function fun(){
+  //   alert("hello");
+  //  }
+
+
+  // var variableName = "hello world";
+  // document.writeln(variableName);
+
+  //  var variableName = "Surya"
+  //  document.writeln(variableName);
+
+  //  var variableName = "Pankaj "
+  //  document.writeln(variableName);
+
+  // document.writeln(a);
+
+
+
+  // let varName1 = "Hello world!";
+  // document.writeln(varName1);
+
+
+  // let varName2 = "Hello world!";
+  // document.writeln(varName2);
+
+  //  varName2 = 67;
+  //  document.writeln(varName2);
+   
+
+
+//   const a = "hello const "
+//   document.writeln(a);
+
+//  a = " hii const"
+//  document.writeln(a);
+
+
+
+
+{
+  var a = "i am var "
+
+ }
+document.writeln(a);
+document.writeln('<br>');
+
+
+
+{
+  let a = "i am let "
+document.writeln(a);
+
+ }
+ 
+{
+  const  a = "i am Const "
+document.writeln(a);
+
+ }
+
+ 
+  
+  
+
+
+  
+
+
+  //  var b = 67;
+  //  document.writeln(b);
+   
+  // let and const
+  
+
+
