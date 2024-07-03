0b19160406fa986550ec4a1e6a892c2d6182bc26
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-03 18:04:31 +0530

    Update Data Types

---
+++ js/class2.js
@@ -0,0 +1,67 @@
+
+// let  a  = "Class 3"
+//  document.writeln(a);
+ 
+// a = "Class 2"
+// document.writeln(a);
+
+// let number  = "9328938";
+// document.writeln(number);
+
+// document.writeln("<br>");
+
+// let numberf  = 9328938.8979;
+// document.writeln(numberf);
+// document.writeln("<br>");
+
+
+// document.writeln( number+numberf + " :  add");
+
+// let Name = null;
+// let a;
+// document.writeln(Name);
+
+// Name = "Ganesh Dutt"
+// document.writeln(Name);
+
+
+  //  Object Data types
+// object 
+let person = {
+ name : "Ganesh Dutt",
+ eyeColor : "Black",
+ height : 5.9
+}
+// for(let x in person) {
+//   document.writeln(x + ": "+ person[x] + "<br>");
+  
+// }
+
+document.writeln(person.name + "😎 "+person.eyeColor );
+
+
+
+
+// array
+let Array  = [89,22,213,43,4,]
+
+
+//  index  0 
+//  0 = 89
+//  1 = 22
+//  2 = 213
+
+document.writeln(Array[2]);
+
+// function 
+// function add(a,b){
+// return a+b
+// }
+
+// let sum = add(10,56)
+// document.writeln(sum);
+
+
+// Date
+let currentDate = new Date();
+document.writeln(currentDate);
