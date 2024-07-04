b033676db7367f6fb64cc8e07068530d8c3748e7
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-04 19:01:11 +0530

    Decision making Statement

---
+++ js/class3.js
@@ -0,0 +1,42 @@
+// let a = 12;
+// let b = 13;
+// let c = a + b;
+// document.writeln(c);
+// let a = 10;
+// let b = 30;
+
+// //  a = a +b ;
+//  a+=b;
+//  document.writeln(a);
+ 
+
+
+// let student = 9;
+//  if(student >= 50){
+// document.writeln("Pass");   
+//  }
+//  else{
+//   document.writeln("Fail");
+  
+//  }
+
+
+
+let studentMarks  = 29;
+if(studentMarks >= 60){
+  document.writeln("First ");
+  
+}
+else if(studentMarks >= 40){
+ document.writeln("Second ");
+ 
+}
+
+else if(studentMarks >= 30){
+  document.writeln("Third ");
+}
+
+else{
+  document.writeln("Fail");
+  
+}
