7b3f0899c85b25fc7eb5abbd048eb10179ae5b79
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-08 18:19:33 +0530

    Switch case

--- js/class4.js
+++ js/class4.js
@@ -1,17 +1,33 @@
-let recharge = 300;
-let youtube = 400;
+// let recharge = 300;
+// let youtube = 400;
 
 
-if(recharge >= 250) {
-  if(youtube>= 350){
-      document.writeln("addfree");
-        }
-        else{
-            document.writeln("Not enough YouTube without add");
-        }
-}
-else{
-    document.writeln("Not Valid Recharge");
+// if(recharge >= 250) {
+//   if(youtube>= 350){
+//       document.writeln("addfree");
+//         }
+//         else{
+//             document.writeln("Not enough YouTube without add");
+//         }
+// }
+// else{
+//     document.writeln("Not Valid Recharge");
+    
+// }
+
+
+let a = 3;
+
+switch(a){
+    case 1: document.writeln("Monday"); break;
+    case 2: document.writeln("Tue");   break;
+    case 3: document.writeln("Wed");break;
+    case 4: document.writeln("Thur");break;
+    case 5: document.writeln("Fri");break;
+    case 6: document.writeln("Sat");break;
+    case 7: document.writeln("Sunday");break;
+
+    default: document.writeln("Invalid Day");
     
 }
 
