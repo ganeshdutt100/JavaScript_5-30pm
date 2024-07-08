749c14ba8ddac32d75b76ca683b64425b121cfb5
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-08 17:56:26 +0530

    Nested if else

---
+++ js/class4.js
@@ -0,0 +1,17 @@
+let recharge = 300;
+let youtube = 400;
+
+
+if(recharge >= 250) {
+  if(youtube>= 350){
+      document.writeln("addfree");
+        }
+        else{
+            document.writeln("Not enough YouTube without add");
+        }
+}
+else{
+    document.writeln("Not Valid Recharge");
+    
+}
+
