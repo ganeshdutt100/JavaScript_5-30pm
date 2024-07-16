c6702e323fc37f9f2107de7beb7b65a7006740fd
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-16 18:23:24 +0530

    Update functions methods

--- js/class7.js
+++ js/class7.js
@@ -6,21 +6,21 @@ document.writeln("class 7");
 
 document.writeln("<br>");
 
-function Emp(id, name) {
-  this.id = id;
-  this.name = name;
-}
+// function Emp(id, name) {
+//   this.id = id;
+//   this.name = name;
+// }
 
-function pEmp(id, name, salary) {
-  this.salary = salary;
-  Emp.call(this, id, name);
-}
-function TEmp(id, name, salary) {
-  pEmp.call(this, id, name, salary);
-}
+// function pEmp(id, name, salary) {
+//   this.salary = salary;
+//   Emp.call(this, id, name);
+// }
+// function TEmp(id, name, salary) {
+//   pEmp.call(this, id, name, salary);
+// }
 
-let a = new pEmp(1, "xyz", 10000);
-let b = new TEmp(12, "dsafas", 827387);
+// let a = new pEmp(1, "xyz", 10000);
+// let b = new TEmp(12, "dsafas", 827387);
 
 // for (let x in a) {
 //   document.writeln(x + " : " + a[x]);
@@ -31,3 +31,35 @@ let b = new TEmp(12, "dsafas", 827387);
 // document.writeln("<br>");
 
 // document.writeln(b.id, " :  ", b.name, " : " + b.salary);
+
+// let arr = [12, 778, 98, 45, 8, 3];
+// let max = Math.max.apply(this, arr);
+// let min = Math.min.apply(this, arr);
+
+// document.writeln(max);
+// document.writeln("<br>");
+
+// document.writeln(min);
+
+// let a = {
+//   name: "js",
+//   functionKey: function () {
+//     return this.name;
+//   },
+// };
+
+// let b = {
+//   name: "React",
+// };
+
+// let c = a.functionKey;
+// let d = c.bind(b);
+// document.writeln(d());
+
+// function add(a, b) {
+//   return a + b;
+// }
+// ("40");
+// let sum = add(10, 30);
+// document.writeln(typeof sum);
+// document.writeln(typeof sum.toString() + " " + sum);
