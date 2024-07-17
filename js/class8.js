9def6ff80d8e42d442da3d120bb11aed46882c72
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-17 18:12:19 +0530

    Update objects

--- js/class8.js
+++ js/class8.js
@@ -1,16 +1,32 @@
 document.writeln("Class 8 Object");
 
-let person = {
-  name: "John",
-  age: 30,
-  city: "New York",
-  sayHello: function () {
-    return "Hello " + this.name;
-  },
-};
+// let person = {
+//   name: "John",
+//   age: 30,
+//   city: "New York",
+//   sayHello: function () {
+//     return "Hello " + this.name;
+//   },
+// };
 
-document.writeln(person.sayHello() + "<br>");
+// document.writeln(person.sayHello() + "<br>");
 
-for (let x in person) {
-  document.writeln(x + " : " + person[x] + "<br>");
+// for (let x in person) {
+//   document.writeln(x + " : " + person[x] + "<br>");
+// }
+
+class classNAMe {
+  constructor(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
+}
+
+document.writeln("<br>");
+
+let person1 = new classNAMe("John", 30, "Developer");
+
+for (let x in person1) {
+  document.writeln(x + " : " + person1[x] + "<br>");
 }
