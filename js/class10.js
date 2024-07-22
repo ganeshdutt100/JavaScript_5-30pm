fadc93d998255759fa73d0e328572db92f119b8c
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-22 18:03:39 +0530

    class 10

---
+++ js/class10.js
@@ -0,0 +1,27 @@
+document.writeln("Class 10" + "<br>");
+
+for (let a = 1; a <= 10; a++) {
+  if (a % 2 == 0) {
+    document.writeln(a + " :  Even" + "<br>");
+  }
+}
+
+document.writeln("<br>");
+
+let b = 1;
+while (b <= 10) {
+  document.writeln(b);
+  b++;
+}
+
+document.writeln("<br>");
+
+let c = 11;
+do {
+  document.writeln(c);
+  c++;
+} while (c <= 10);
+
+// 2*1=2
+// 2*2=4
+// 2*3=6
---
+++ js/class9.js
@@ -0,0 +1,18 @@
+document.writeln("Class 9 ");
+document.writeln("<br>");
+
+// for loop
+// for (let a = 0; a <= 10; a++) {
+//   document.writeln(a + "<br>");
+// }
+
+// While loop
+// - The while loop continues executing a block of code as long as a specified
+// condition is true. The condition is checked before each iteration.
+document.writeln("<br>");
+
+// let b = 0;
+// while (b <= 10) {
+//   console.log(b);
+//   b++;
+// }
