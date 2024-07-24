a1ce10fa315310033182c15b902eb3664896cf0b
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-24 18:14:31 +0530

    String

---
+++ js/class12.js
@@ -0,0 +1,7 @@
+document.writeln("Class 12" + "<br>");
+
+let name = "String";
+let str = "I am " + name + " Hello";
+let str2 = `I am : ${name} hello`;
+document.writeln(str + "<br>");
+document.writeln(str2);
