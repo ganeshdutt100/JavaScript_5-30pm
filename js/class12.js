858528a65094f19e21eeda154b8df6a6731c0887
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-24 18:48:30 +0530

    String

--- js/class12.js
+++ js/class12.js
@@ -1,7 +1,51 @@
 document.writeln("Class 12" + "<br>");
 
-let name = "String";
-let str = "I am " + name + " Hello";
-let str2 = `I am : ${name} hello`;
-document.writeln(str + "<br>");
-document.writeln(str2);
+// let name = "String";
+// let str = "I am " + name + " Hello";
+// let str2 = `I am : ${name} hello`;
+// document.writeln(str + "<br>");
+
+// let str = "hello";
+// let str2 = " world";
+// let str3 = " Hey";
+
+// concatenation;
+// document.writeln(str.concat(str2).concat(str3));
+// document.writeln(str + str2);
+
+let str = " Hello WorLd! ";
+// let str2 = str.indexOf("l");
+// let str2 = str.lastIndexOf("l");
+let str2 = str.replace(/l/gi, "a");
+console.log(str2);
+
+// let str2 = str.trim();
+// console.log(str.trim().length);
+// console.log(str2.length);
+
+// upper and lower case-----
+// let output = str.toUpperCase();
+// let output = str.toLowerCase();
+// console.log(output);
+
+// String Splitting:
+// let str2 = "Apple orange banana";
+// let output = str2.split(" ");
+
+// document.writeln(str2 + "<br>");
+// // document.writeln(output);
+// console.log(str2);
+
+// console.log(output);
+
+// substring----
+// let output = str.substring(6, 12);
+// document.writeln(output);
+
+// Accessing Characters:
+// let output = str[7];
+// document.writeln(output);
+
+// length
+// let Length = str.length;
+// document.writeln(str + " : " + Length);
