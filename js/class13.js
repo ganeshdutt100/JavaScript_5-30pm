b6eb6ccd202b6dff3932833d480e58513e54eb70
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-31 18:00:47 +0530

    Update Array

---
+++ js/class13.js
@@ -0,0 +1,15 @@
+document.writeln("class 13");
+
+// let a = ["a", "b", "c", 78, "d", "e", "f", "g", "h", "i"];
+// console.log(a);
+
+// let fruits = new Array();
+// fruits[0] = "a";
+// fruits[1] = "b";
+// fruits[2] = "c";
+// fruits[3] = "d";
+
+// console.log(fruits);
+let fruits = new Array("a", "b", "c", 78, "d", "e", "f", "g", "h", "i");
+
+console.log(fruits);
