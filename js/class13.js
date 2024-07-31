472a81dc7907fa5e31d5755b4fcec6db8c1c60c5
Ganesh Dutt <gdutt2304@gmail.com>
2024-07-31 18:26:57 +0530

    Array Methods

--- js/class13.js
+++ js/class13.js
@@ -10,6 +10,30 @@ document.writeln("class 13");
 // fruits[3] = "d";
 
 // console.log(fruits);
-let fruits = new Array("a", "b", "c", 78, "d", "e", "f", "g", "h", "i");
+// let fruits = new Array("a", "b", "c", 78, "d", "e", "f", "g", "h", "i");
 
-console.log(fruits);
+// console.log(fruits);
+
+let a = ["a", "b", "c", "d", "e", "f", "g"];
+a.reverse();
+console.log(a);
+
+// let fruits = [
+//   "Apple", //0
+//   "Orange", //1
+//   "Banana", //2
+//   "Kiwi",
+// ]; //3
+
+// splice(start , deleteCount , add)
+// fruits.splice(1, 0, "grape", "melon");
+// a.pop();
+// let num = [9, 7, 5, 0, 3, 2, 6];
+// num.sort();
+// a.push("Hello");
+
+// a.shift();
+// a.unshift("World");
+// a.fill(0, 3, 10);
+// console.log(a);
+// console.log(fruits);
