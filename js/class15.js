81c3a5fd99c6f70811170e76de1ed9a0df64eb33
Ganesh Dutt <gdutt2304@gmail.com>
2024-08-05 19:04:16 +0530

    Update methods

--- js/class15.js
+++ js/class15.js
@@ -6,15 +6,25 @@ let a = [10, 34, 76, 89, 67, 43, 23];
 // let output = a.some((x) => x % 2 == 0);
 // let output = a.filter((x) => x % 2 != 0);
 // let output = a.find((x) => x % 2 == 0);
-let output = a.findIndex((x) => x % 2 == 0);
+// let output = a.findIndex((x) => x % 2 == 0);
+// let output = a.map((x) => x + 4);
+// let output = a.reduce((acc, curr) => acc + curr);
 
-console.log(output);
+// console.log(output);
+// console.log(a);
 
-// let arr = ["a", "b", "c", "d", "e", "f", "g", "h"];
+let valueArray = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 // for (let ouputArr of arr.entries()) {
 //   console.log(ouputArr);
 // }
+for (let outputValue of valueArray.keys()) {
+  console.log(outputValue);
+}
+
+for (let outputValue of valueArray.values()) {
+  console.log(outputValue);
+}
 
 // console.log(arr);
 
