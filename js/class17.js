c3639f34165cd61976a527c549a271cc404801a1
Ganesh Dutt <gdutt2304@gmail.com>
2024-08-08 17:21:35 +0530

    Update Calculator

--- js/class17.js
+++ js/class17.js
@@ -1,1 +1,6 @@
-document.writeln("Class 17");
+document.writeln("Class 17" + "<br>");
+var arr = [5, 7, 3, 2];
+var max = Math.max.apply(this, arr);
+var min = Math.min.apply(this, arr);
+document.writeln(max);
+document.writeln(min);
