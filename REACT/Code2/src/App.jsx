6934d18ddf12eb58d7efec2a0bd3c7cfad2c68f8
unknown <gdutt2304@gmail.com>
2024-09-12 16:21:20 +0530

    React

---
+++ REACT/Code2/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <>
+    Hello world!
+
+    <p>Lorem ipsum dolor sit amet.</p>
+
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, pariatur. Doloremque velit deserunt magnam quasi voluptatibus accusamus quidem animi similique. Culpa tempora expedita voluptatem dolor debitis possimus cum nemo perspiciatis?</p>
+   </>
+   
+  )
+}
+
+export default App
---
+++ REACT/reactCoding/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+ 
+ return (
+    <div>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas in deserunt numquam accusantium nesciunt ipsum iste repudiandae porro minima praesentium, libero, consequuntur eaque dolorem ratione facilis, molestias vel neque voluptatum?</p>
+
+       <h1>Lorem ipsum dolor sit amet.</h1>
+    </div>
+  )
+}
+
+export default App
