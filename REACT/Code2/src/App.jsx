6b90e8e88f52a2d1edbbe081e201c406dbad65ba
unknown <gdutt2304@gmail.com>
2024-09-12 16:55:13 +0530

    Update React components

--- REACT/Code2/src/App.jsx
+++ REACT/Code2/src/App.jsx
@@ -1,17 +1,27 @@
 
 import './App.css'
+import { Navbar } from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import { Section } from './components/Section'
 
 function App() {
   
-
+// const nameValue  = "Ganesh Dutt";
+// const image  = <img src='https://img.freepik.com/free-photo/happy-excited-tourist-shooting-landmarks_1262-18852.jpg?ga=GA1.1.226300517.1725278774&semt=ais_hybrid'></img>
   return (
    <>
+    {/* <h1>{nameValue}</h1>
+    <p>{image}</p>
     Hello world!
 
     <p>Lorem ipsum dolor sit amet.</p>
 
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, pariatur. Doloremque velit deserunt magnam quasi voluptatibus accusamus quidem animi similique. Culpa tempora expedita voluptatem dolor debitis possimus cum nemo perspiciatis?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, pariatur. Doloremque velit deserunt magnam quasi voluptatibus accusamus quidem animi similique. Culpa tempora expedita voluptatem dolor debitis possimus cum nemo perspiciatis?</p> */}
+
+    <Navbar></Navbar>
+    <Section/>
    </>
    
   )
