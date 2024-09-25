53918f038d1194e35690648e02f5daea2ca0668b
unknown <gdutt2304@gmail.com>
2024-09-25 16:45:16 +0530

    Add Background Color

--- REACT/code6/src/components/Task1.jsx
+++ REACT/code6/src/components/Task1.jsx
@@ -2,10 +2,41 @@ import React, { useState } from "react";
 
 const Task1 = () => {
   const [on, setOn] = useState(false);
+
+  const styleFun = () => {
+    if (on) {
+      return {
+        backgroundColor: "red",
+        color: "white",
+        padding: "40px 90px",
+        borderRadius: "10px",
+      };
+    } else {
+      return {
+        backgroundColor: "yellow",
+        color: "black",
+        padding: "40px 90px",
+        borderRadius: "10px",
+      };
+    }
+  };
+
+  //   let a =
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti eaque iusto dolorem ducimus eum quisquam ratione est sit nostrum, dolore animi eligendi aut beatae voluptates fugit magnam, enim similique itaque.";
+
   return (
     <div>
-      <h1>{on ? "ON" : "OFF"}</h1>
-      <button onClick={() => setOn(!on)}>{on ? "OFF" : "ON"}</button>
+      {/* <button onClick={() => setOn(!on)}>{on ? "Hide" : "Show"}</button>
+
+      <p>{on ? a : " "}</p> */}
+
+      {/* <h1>{on ? "ON" : "OFF"}</h1>
+      <button onClick={() => setOn(!on)}>{on ? "OFF" : "ON"}</button> */}
+
+      <div className="div" style={styleFun()}>
+        <h1>{on ? "Red" : "Yellow"}</h1>
+        <button onClick={() => setOn(!on)}>{on ? "Yellow" : "Red"}</button>
+      </div>
     </div>
   );
 };
